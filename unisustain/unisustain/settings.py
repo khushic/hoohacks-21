29737bfd781e094fe44b807f551aa499ea8539c7
Prateek Gunnam <pgunnam@gmail.com>
2021-03-28 00:43:13 -0400

    styling jaunt

--- unisustain/unisustain/settings.py
+++ unisustain/unisustain/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'unisustain',
-        'USER': 'khush',
+        'USER': 'root',
         'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '3306',
@@ -125,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static')
+]
 
 CRIPSY_TEMPLATE_PACK='bootstrap4'
 
